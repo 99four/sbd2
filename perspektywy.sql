6cf03b52055a23f8e6ee8d80d8cecf36f60a003b
Damian Bachórz <damian.bachorz@briisk.co>
2016-10-13 21:59:23 +0200

    zad 3

--- perspektywy.sql
+++ perspektywy.sql
@@ -1,12 +1,11 @@
 --zad 1
-
 CREATE VIEW ASYSTENCI (ID, NAZWISKO, PLACA, STAŻ_PRACY) AS
-  SELECT id_prac, nazwisko, placa_pod + nvl(placa_dod, 0),
-  'lat: '|| EXTRACT(year FROM (SYSDATE - ZATRUDNIONY) YEAR TO MONTH) 
-  || ' miesięcy: ' ||
-  EXTRACT(MONTH FROM (SYSDATE - ZATRUDNIONY) YEAR TO MONTH)
-  FROM pracownicy
-  WHERE ETAT='ASYSTENT';
+SELECT id_prac, nazwisko, placa_pod + nvl(placa_dod, 0),
+'lat: '|| EXTRACT(year FROM (SYSDATE - ZATRUDNIONY) YEAR TO MONTH) 
+|| ' miesięcy: ' ||
+EXTRACT(MONTH FROM (SYSDATE - ZATRUDNIONY) YEAR TO MONTH)
+FROM pracownicy
+WHERE ETAT='ASYSTENT';
 
 --zad 2
 create view place (id_zesp, srednia, minimum, maximum, fundusz, l_pensji, l_dodatkow)
@@ -21,3 +20,18 @@ from pracownicy p
 join zespoly z
 on p.id_zesp = z.id_zesp 
 group by p.id_zesp order by p.id_zesp;
+
+--zad 3
+select w.nazwisko, w.placa_pod 
+from place p join pracownicy w on p.id_zesp = w.id_zesp 
+where w.placa_pod + nvl(w.placa_dod, 0) < p.srednia 
+order by w.nazwisko;
+
+
+
+
+
+
+
+
+
