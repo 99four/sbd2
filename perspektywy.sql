ea713ae24852ad843f70fbb5100f310881d4e06c
Damian Bachórz <damian.bachorz@briisk.co>
2016-10-13 22:33:15 +0200

    next

--- perspektywy.sql
+++ perspektywy.sql
@@ -1,5 +1,5 @@
 --zad 1
-CREATE VIEW ASYSTENCI (ID, NAZWISKO, PLACA, STAŻ_PRACY) AS
+CREATE OR REPLACE VIEW ASYSTENCI (ID, NAZWISKO, PLACA, STAŻ_PRACY) AS
 SELECT id_prac, nazwisko, placa_pod + nvl(placa_dod, 0),
 'lat: '|| EXTRACT(year FROM (SYSDATE - ZATRUDNIONY) YEAR TO MONTH) 
 || ' miesięcy: ' ||
@@ -8,7 +8,7 @@ FROM pracownicy
 WHERE ETAT='ASYSTENT';
 
 --zad 2
-create view place (id_zesp, srednia, minimum, maximum, fundusz, l_pensji, l_dodatkow)
+create or replace view place (id_zesp, srednia, minimum, maximum, fundusz, l_pensji, l_dodatkow)
 	as select p.id_zesp, 
 avg(p.placa_pod + nvl(p.placa_dod, 0)), 
 min(p.placa_pod + nvl(p.placa_dod, 0)), 
@@ -27,8 +27,11 @@ from place p join pracownicy w on p.id_zesp = w.id_zesp
 where w.placa_pod + nvl(w.placa_dod, 0) < p.srednia 
 order by w.nazwisko;
 
-
-
+--zad 4
+create or replace view place_minimalne as
+select id_prac, nazwisko, etat, placa_pod 
+from pracownicy where placa_pod < 700
+with check OPTION constraint za_wysoka_placa;
 
 
 
